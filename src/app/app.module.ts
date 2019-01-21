071a6eb8986dee4c80ce65e304f4b59d80eed25a
volkov-v <volkov.vladyslav.2018@gmail.com>
2019-01-21 15:10:14 +0200

    added dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ListTableComponent } from './list-table/list-table.component';
 import { ImagesComponent } from './images/images.component';
 import { PostTilesComponent } from './post-tiles/post-tiles.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule, MatTableModule } from '@angular/material';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { GalleriaModule } from 'primeng/galleria';
+import { AccordionModule } from 'primeng/accordion';
 
 @NgModule({
   declarations: [
@@ -20,7 +26,13 @@ import { NavigationComponent } from './navigation/navigation.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatTableModule,
+    MDBBootstrapModule.forRoot(),
+    GalleriaModule,
+    AccordionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
